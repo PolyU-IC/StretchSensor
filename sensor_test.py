8b73778f703ac0b1e75f7f85fc5c666db8519e7e
ahing997 <38163712+ahing997@users.noreply.github.com>
2022-04-08 18:41:11 +0800

    Add files via upload

---
+++ sensor_test.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import serial
+import time
+import matplotlib.pyplot as plt
+import msvcrt
+import numpy as np
+from datetime import datetime
+
+s = serial.Serial(
+    port="COM3", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE
+)
+s.write(str.encode("AT"))
+data=[]
+state=0
+x1=0
+x2=0
+done=True
+Num=0
+start = datetime.now()
+current_time = start.strftime("%H:%M:%S")
+print("Current Time =", current_time)
+while (Num < 5000):
+    res = s.read(1)
+    if (state==0) & (res==b'\xaa'):
+        state=1
+    elif (state==1) & (res==b'\xfa'):
+        state=2
+    elif (state==2):
+        x1=int.from_bytes(res,"little")
+        state=3
+    else:
+        x2=int.from_bytes(res,"little")
+        state=0
+        result=256*x1+x2
+        data.append(result)
+        print(Num,result)
+        Num=Num+1
+#    print(res)
+s.close()
+end = datetime.now()
+current_time = end.strftime("%H:%M:%S")
+print("Current Time =", current_time)
+print(end-start)
+
+ndata=np.array(data)
+mydata=ndata[0:2000]
+plt.plot(mydata[(mydata > 6700) & (mydata<6850)])
+mydata=ndata[2500:4000]
+plt.plot(mydata[(mydata > 6700) & (mydata<6850)])
+mydata=ndata[4000:5000]
+plt.plot(mydata[(mydata > 6700) & (mydata<6850)])
+
+f = open("my_sensor.csv", "w")
+for v in ndata:
+    f.write("%s\n" % v)
+f.close()
